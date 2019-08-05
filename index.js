79e98fac169641f8c371023f02e4e1b6b04f0ad9
Harum Helmy <harumendhah@gmail.com>
2019-08-05 19:29:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 function findMatching(drivers, string) {
-    let match = drivers.filter(driver => 
-        driver.toLowerCase() === string.toLowerCase())
+    let match = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
     return match
 }
 
 function fuzzyMatch(drivers, string) {
-    let match = drivers.filter(driver =>
-        driver.substring(0, string.length) === string) 
+    let match = drivers.filter(driver => driver.substring(0, string.length) === string) 
     return match 
 }
 
 function matchName(drivers, string) {
-    let match = drivers.filter(function (el) {
-        return el.name === string 
-    })
+    let match = drivers.filter(function (el) {return el.name === string })
     return match 
 }
-
-fuzzyMatch();
